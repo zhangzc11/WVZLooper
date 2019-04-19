743b6fab1b533f0a6349d08770c89395b0c27629
Philip Chang <sgnoohc@gmail.com>
2019-04-19 00:12:04 -0700

    Moved rest of the VH portion after splitting into 'rare' cateogry

--- scripts/makeplot.py
+++ scripts/makeplot.py
@@ -10,9 +10,7 @@ bkgfiles = [
         "outputs/wz.root",
         "outputs/twz.root",
         "outputs/rare.root",
-        "outputs/vh.root",
         "outputs/dyttbar.root",
-        "outputs/MC_ttw_lnu_amcatnlo_1_results.root",
         ]
 sigfiles = [
         "outputs/wwz.root",
@@ -29,12 +27,12 @@ allfiles = [ f for f in allfiles if "ttz" not in f ]
 allfiles = [ f for f in allfiles if "_zz_" not in f ]
 allfiles = [ f for f in allfiles if "_wz_" not in f ]
 
-colors = [2005, 2001, 2003, 2007, 920, 2010, 2012]
+colors = [2005, 2001, 2003, 2007, 920, 2012]
 
 p.dump_plot(fnames=bkgfiles,
         sig_fnames=sigfiles,
         usercolors=colors,
-        legend_labels=["ttz", "zz", "wz", "twz", "rare", "vh", "Z/t#bar{t}"],
+        legend_labels=["t#bar{t}Z", "ZZ", "WZ", "tWZ", "other", "Z/t#bar{t}"],
         signal_labels=["WWZ", "WZZ", "WWW", "ZZZ", "VVV"],
         dirname="plots/lin",
         filter_pattern="__",
@@ -43,9 +41,10 @@ p.dump_plot(fnames=bkgfiles,
             "print_yield":True,
             "nbins":15,
             "signal_scale": 1,
-            "legend_scalex":1.4,
+            "legend_scalex":1.8,
             "legend_scaley":1.1,
             "legend_ncolumns": 3,
+            "ymax_scale": 1.2,
             "lumi_value":137
             }
         )
@@ -62,9 +61,10 @@ p.dump_plot(fnames=bkgfiles,
             "print_yield":True,
             "nbins":15,
             "signal_scale": 1,
-            "legend_scalex":1.4,
+            "legend_scalex":1.8,
             "legend_scaley":1.1,
             "legend_ncolumns": 3,
+            "ymax_scale": 1.2,
             "lumi_value":137
             }
         )

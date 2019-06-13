d822f8c83eafba3fda615b9f4a1871b7dae99105
Philip Chang <sgnoohc@gmail.com>
2019-06-12 18:20:18 -0700

    update

---
+++ scripts/pretty_print.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Usage:"
+    echo ""
+    echo "   $ $0 ntuple_version tagname"
+    echo ""
+    echo ""
+    exit
+}
+
+if [ -z $1 ]; then usage; fi
+if [ -z $2 ]; then usage; fi
+
+if [ ! -d plots/$1/$2 ]; then
+    echo ">>> Error! cannot find outputs/$1/$2"
+    echo ">>> Check your ntuple_version and tagname that you've provided!"
+    exit
+fi
+
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow/ChannelOffZHighMET_cutflow.txt > offz.txt
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow/ChannelOnZNjet_cutflow.txt > onz.txt
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow/ChannelEMuHighMll_cutflow.txt > emu.txt
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow_detail/ChannelOffZHighMET_cutflow.txt > offz_detail.txt
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow_detail/ChannelOnZNjet_cutflow.txt > onz_detail.txt
+python rooutil/yield_for_keynote.py plots/$1/$2/cutflow_detail/ChannelEMuHighMll_cutflow.txt > emu_detail.txt

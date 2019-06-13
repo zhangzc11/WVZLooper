8112abc412ef842e95fbd5b453e079fac04a952a
Zhicai <zzhang2@caltech.edu>
2019-06-13 14:37:40 -0700

    update 3lepton scripts

--- scripts/hadd.sh
+++ scripts/hadd.sh
@@ -55,6 +55,29 @@ elif [[ $1 == *"WVZ"* ]]; then
     fi
     hadd -f higgs.root MC_ggh_hzz4l_powheg_1_results.root MC_vh_nonbbwwzz_amcatnlo_1_results.root MC_tth_nonbb_powheg_1_results.root
     hadd -f data.root DATA_data_Run*.root
+elif [[ $1 == *"Trilep"* ]]; then
+    hadd -f zz.root MC_zz_4l_powheg_1_results.root MC_zz_2l2q_powheg_1_results.root MC_zz_2l2v_powheg_1_results.root MC_ggzz_*.root
+    hadd -f ttz.root MC_ttz_ll_mll1_amcatnlo_1_results.root MC_ttz_llvv_mll10_amcatnlo_1_results.root
+    hadd -f twz.root MC_twz_ll_madgraph_1_results.root
+    hadd -f wz.root MC_wz_3lv_amcatnlo_1_results.root
+    hadd -f sig.root MC_www_amcatnlo_1_results.root MC_wwz_amcatnlo_1_results.root MC_wzz_amcatnlo_1_results.root MC_zzz_amcatnlo_1_results.root
+    hadd -f wwz.root MC_wwz_amcatnlo_1_results.root
+    hadd -f www.root MC_www_amcatnlo_1_results.root
+    hadd -f wzz.root MC_wzz_amcatnlo_1_results.root
+    hadd -f zzz.root MC_zzz_amcatnlo_1_results.root
+    hadd -f nonh_wwz.root MC_wwz_amcatnlo_1_results.root
+    hadd -f nonh_www.root MC_www_amcatnlo_1_results.root
+    hadd -f nonh_wzz.root MC_wzz_amcatnlo_1_results.root
+    hadd -f nonh_zzz.root MC_zzz_amcatnlo_1_results.root
+    #hadd -f other.root MC_dy_m1050_madgraph_1_results.root MC_dy_m50_madgraph_1_results.root MC_ggh_hzz4l_powheg_1_results.root MC_sts_4f_leptonic_madgraph_1_results.root MC_tZq_ll_madgraph_1_results.root MC_ttbar_amcatnlo_1_results.root MC_ttw_lnu_amcatnlo_1_results.root MC_tw_antitopnofullhad_powheg_1_results.root MC_tw_topnofullhad_powheg_1_results.root MC_vbsww_madgraph_1_results.root MC_wjets_incl_madgraph_1_results.root MC_ww_incl_pythia_1_results.root MC_wwg_amcatnlo_1_results.root MC_wzg_amcatnlo_1_results.root
+    hadd -f dyttbar.root MC_dy_m1050_madgraph_1_results.root MC_dy_m50_madgraph_1_results.root MC_ttbar_dilep_madgraph_1_results.root MC_zg_llg_amcatnlo_1_results.root
+    if [[ $1 == *"0.0.5"* ]]; then # older samples had vbsww and ww_inclusive
+        hadd -f rare.root MC_sts_4f_leptonic_madgraph_1_results.root MC_tZq_ll_madgraph_1_results.root MC_ttw_lnu_amcatnlo_1_results.root MC_tw_antitopnofullhad_powheg_1_results.root MC_tw_topnofullhad_powheg_1_results.root MC_ww_incl_pythia_1_results.root MC_wwg_amcatnlo_1_results.root MC_wzg_amcatnlo_1_results.root MC_vbsww_madgraph_1_results.root
+    else # newer samples don't have vbsww and ww is dilepton filtered
+        hadd -f rare.root MC_sts_4f_leptonic_madgraph_1_results.root MC_tZq_ll_madgraph_1_results.root MC_ttw_lnu_amcatnlo_1_results.root MC_tw_antitopnofullhad_powheg_1_results.root MC_tw_topnofullhad_powheg_1_results.root MC_ww_2l_powheg_1_results.root MC_wwg_amcatnlo_1_results.root MC_wzg_amcatnlo_1_results.root
+    fi
+    hadd -f higgs.root MC_ggh_hzz4l_powheg_1_results.root MC_tth_nonbb_powheg_1_results.root
+    hadd -f data.root DATA_data_Run*.root
 fi
 
 cd - > /dev/null

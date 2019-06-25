03df6289fb1a4d8404a591bbd2a9603ee1f91e0c
Philip Chang <sgnoohc@gmail.com>
2019-06-25 11:46:49 -0700

    update

--- scripts/write_datacards.py
+++ scripts/write_datacards.py
@@ -2,6 +2,7 @@
 
 import ROOT as r
 
+import pyrootutil as pr
 import plottery_wrapper as p
 import argparse
 import sys
@@ -15,26 +16,44 @@ parser.add_argument('-t' , '--tag'             , dest='tag'             , help='
 
 args = parser.parse_args()
 
-# Make plot function of the main analysis
-# The main thing is that this plotting script runs over the output files
-# that was ran with "WVZ201*_v*" ntuples
-# This means that it assumes specific histogram files to exists
-
 ntuple_version = args.sample_set_name
 tag = args.tag
 
-files = [
-        "outputs/{}/{}/sig.root".format(ntuple_version, tag),
-        "outputs/{}/{}/ttz.root".format(ntuple_version, tag),
-        "outputs/{}/{}/zz.root".format(ntuple_version, tag),
-        "outputs/{}/{}/wz.root".format(ntuple_version, tag),
-        "outputs/{}/{}/twz.root".format(ntuple_version, tag),
-        "outputs/{}/{}/rare.root".format(ntuple_version, tag),
-        "outputs/{}/{}/dyttbar.root".format(ntuple_version, tag),
-        "outputs/{}/{}/higgs.root".format(ntuple_version, tag),
-        ]
+fname_sig     = "outputs/{}/{}/sig.root".format(ntuple_version, tag)
+fname_ttz     = "outputs/{}/{}/ttz.root".format(ntuple_version, tag)
+fname_zz      = "outputs/{}/{}/zz.root".format(ntuple_version, tag)
+fname_wz      = "outputs/{}/{}/wz.root".format(ntuple_version, tag)
+fname_twz     = "outputs/{}/{}/twz.root".format(ntuple_version, tag)
+fname_rare    = "outputs/{}/{}/rare.root".format(ntuple_version, tag)
+fname_dyttbar = "outputs/{}/{}/dyttbar.root".format(ntuple_version, tag)
+fname_higgs   = "outputs/{}/{}/higgs.root".format(ntuple_version, tag)
+fname_data    = "outputs/{}/{}/data.root".format(ntuple_version, tag)
+
+files = [fname_sig, fname_ttz, fname_zz, fname_wz, fname_twz, fname_rare, fname_dyttbar, fname_higgs]
+nonzzbkg = [fname_sig, fname_ttz, fname_wz, fname_twz, fname_rare, fname_dyttbar, fname_higgs]
+nonttzbkg = [fname_sig, fname_zz, fname_wz, fname_twz, fname_rare, fname_dyttbar, fname_higgs]
 
 ofile = r.TFile("stat_input.root", "recreate")
+
+##########################
+# OnZ Control region gmN syst line
+##########################
+
+onz_zz_h = pr.get_summed_histogram([fname_zz], "ChannelOnZ__Yield")
+onz_data_h = pr.get_summed_histogram([fname_data], "ChannelOnZ__Yield")
+onz_nonzz_h = pr.get_summed_histogram(nonzzbkg, "ChannelOnZ__Yield")
+zz_sf = pr.get_sf(onz_zz_h, onz_data_h, onz_nonzz_h).GetBinContent(1)
+expected_nevt_zz = onz_data_h.GetBinContent(1)
+
+##########################
+# BTag Control region gmN syst line
+##########################
+
+bcr_ttz_h = pr.get_summed_histogram([fname_ttz], "ChannelBTagEMu__Yield")
+bcr_data_h = pr.get_summed_histogram([fname_data], "ChannelBTagEMu__Yield")
+bcr_nonttz_h = pr.get_summed_histogram(nonttzbkg, "ChannelBTagEMu__Yield")
+ttz_sf = pr.get_sf(bcr_ttz_h, bcr_data_h, bcr_nonttz_h).GetBinContent(1)
+expected_nevt_ttz = bcr_data_h.GetBinContent(1)
 
 ##########################
 # EMu channel
@@ -83,6 +102,12 @@ for index, s in enumerate(files):
         d.Write()
         hists.append(d)
 
+    if index == 2: # this is ttZ process
+        hists[2].Scale(ttz_sf)
+
+    if index == 3: # this is ZZ process
+        hists[3].Scale(zz_sf)
+
     # Yields
     print procname, h.Integral()
 
@@ -94,9 +119,32 @@ for index, s in enumerate(files):
     ofile.cd()
     hists[-1].Write()
 
+systs = []
+
+# ZZ CR systematic line
+onz_cr_hist = r.TH1F("onz_cr", "", 5, 0, 5)
+onz_cr_hist.SetBinContent(1, expected_nevt_zz)
+onz_cr_hist.SetBinContent(2, expected_nevt_zz)
+onz_cr_hist.SetBinContent(3, expected_nevt_zz)
+onz_cr_hist.SetBinContent(4, expected_nevt_zz)
+onz_cr_hist.SetBinContent(5, expected_nevt_zz)
+alpha = hists[3].Clone("alpha") # The index 3 == zz (the order is data, sig, ttz, zz, ...)
+alpha.Divide(onz_cr_hist)
+systs.append( ("CRZZ", "gmN", [onz_cr_hist], {"emu_sig":0, "emu_ttz":0, "emu_zz":[ "{:4f}".format(alpha.GetBinContent(i)) for i in range(1,6) ], "emu_wz":0, "emu_twz":0, "emu_rare":0, "emu_dyttbar":0, "emu_higgs":0}) )
+
+# ttZ CR systematic line
+btag_cr_hist = r.TH1F("btag_cr", "", 5, 0, 5)
+btag_cr_hist.SetBinContent(1, expected_nevt_ttz)
+btag_cr_hist.SetBinContent(2, expected_nevt_ttz)
+btag_cr_hist.SetBinContent(3, expected_nevt_ttz)
+btag_cr_hist.SetBinContent(4, expected_nevt_ttz)
+btag_cr_hist.SetBinContent(5, expected_nevt_ttz)
+alpha = hists[2].Clone("alpha") # The index 2 == ttz (the order is data, sig, ttz, zz, ...)
+alpha.Divide(btag_cr_hist)
+systs.append( ("CRTTZ", "gmN", [btag_cr_hist], {"emu_sig":0, "emu_ttz":[ "{:4f}".format(alpha.GetBinContent(i)) for i in range(1,6) ], "emu_zz":0, "emu_wz":0, "emu_twz":0, "emu_rare":0, "emu_dyttbar":0, "emu_higgs":0}) )
+
 # Now create data card writer
-# bkg2 does not need stat error as it is taken care of by CR stats
-d = dw.DataCardWriter(sig=hists[1], bgs=hists[2:], data=hists[0])
+d = dw.DataCardWriter(sig=hists[1], bgs=hists[2:], data=hists[0], systs=systs, no_stat_procs=["emu_zz", "emu_ttz"])
 
 d.set_bin(1)
 d.set_region_name("bin1")
@@ -151,6 +199,12 @@ for index, s in enumerate(files):
         d.Write()
         hists.append(d)
 
+    if index == 2: # this is ttZ process
+        hists[2].Scale(ttz_sf)
+
+    if index == 3: # this is ZZ process
+        hists[3].Scale(zz_sf)
+
     # Yields
     print procname, h.Integral()
 
@@ -162,10 +216,76 @@ for index, s in enumerate(files):
     ofile.cd()
     hists[-1].Write()
 
+systs = []
+
+# If procname == zz
+onz_cr_hist = r.TH1F("onz_cr", "", 1, 0, 1)
+onz_cr_hist.SetBinContent(1, expected_nevt_zz)
+alpha = hists[3].Clone("alpha") # The index 3 == zz (the order is data, sig, ttz, zz, ...)
+alpha.Divide(onz_cr_hist)
+systs.append( ("CRZZ", "gmN", [onz_cr_hist], {"offz_sig":0, "offz_ttz":0, "offz_zz":[ "{:4f}".format(alpha.GetBinContent(1)) ], "offz_wz":0, "offz_twz":0, "offz_rare":0, "offz_dyttbar":0, "offz_higgs":0}) )
+
+# ttZ CR systematic line
+btag_cr_hist = r.TH1F("btag_cr", "", 1, 0, 1)
+btag_cr_hist.SetBinContent(1, expected_nevt_ttz)
+alpha = hists[2].Clone("alpha") # The index 2 == ttz (the order is data, sig, ttz, zz, ...)
+alpha.Divide(btag_cr_hist)
+systs.append( ("CRTTZ", "gmN", [btag_cr_hist], {"offz_sig":0, "offz_ttz":[ "{:4f}".format(alpha.GetBinContent(1)) ], "offz_zz":0, "offz_wz":0, "offz_twz":0, "offz_rare":0, "offz_dyttbar":0, "offz_higgs":0}) )
+
 # Now create data card writer
-# bkg2 does not need stat error as it is taken care of by CR stats
-d = dw.DataCardWriter(sig=hists[1], bgs=hists[2:], data=hists[0])
+d = dw.DataCardWriter(sig=hists[1], bgs=hists[2:], data=hists[0], systs=systs, no_stat_procs=["offz_zz", "offz_ttz"])
 
 d.set_bin(1)
 d.set_region_name("bin1")
 d.write("stats/offz_datacard_bin1.txt")
+
+##########################
+# OnZ channel
+##########################
+
+tfiles = []
+procnames = []
+hists = []
+
+for index, s in enumerate(files):
+
+    # ROOT file
+    f = r.TFile(s)
+    tfiles.append(f)
+
+    # Name of the process
+    basename = os.path.basename(s)
+    procname = basename.replace(".root", "")
+
+    # List of procnames
+    procnames.append(procname)
+
+    # EMu channel
+    h = f.Get("ChannelOnZ__Yield")
+
+    # If index == 0 then write data
+    if index == 0:
+        d = h.Clone("onz_data_obs")
+        d.Reset()
+        d.SetBinContent(1, 4)
+        ofile.cd()
+        d.Write()
+        hists.append(d)
+
+    # Yields
+    print procname, h.Integral()
+
+    # Aggregate histograms
+    hists.append(h.Clone("onz_" + procname))
+    hists[-1].SetTitle("onz_" + procname)
+
+    # Write to ofile
+    ofile.cd()
+    hists[-1].Write()
+
+# Now create data card writer
+d = dw.DataCardWriter(sig=hists[1], bgs=hists[2:], data=hists[0])
+
+d.set_bin(1)
+d.set_region_name("bin1")
+d.write("stats/onz_datacard_bin1.txt")
